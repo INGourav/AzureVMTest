eb1f4db74437a209cda044bc3bd9db636bc7e030
Gourav Kumar <33678627+unpredictablegourav@users.noreply.github.com>
2019-09-24 20:51:12 +0530

    Create UnManagedDisk.ps1

---
+++ UnManagedDisk.ps1
@@ -0,0 +1,18 @@
+Set-AzureRmContext -Subscription "SUBSCRIPTION NAME"
+$storageAccounts = Get-AzureRmStorageAccount
+ foreach($storageAccount in $storageAccounts){
+    $storageKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $storageAccount.ResourceGroupName -Name $storageAccount.StorageAccountName)[0].Value
+      $context = New-AzureStorageContext -StorageAccountName $storageAccount.StorageAccountName -StorageAccountKey $storageKey
+       $containers = Get-AzureStorageContainer -Context $context
+ foreach($container in $containers){
+    $blobs = Get-AzureStorageBlob -Container $container.Name -Context $context
+     $blobs | Where-Object {$_.BlobType -eq 'PageBlob' -and $_.Name.EndsWith('.vhd')} | ForEach-Object { 
+      if($_.ICloudBlob.Properties.LeaseStatus -eq 'Unlocked'){
+     $blobs | Select * | Export-Csv C:\temp\unmanagedunattached.csv -append -notypembuormation
+      }
+else{
+     $blobs | Select * | Export-Csv C:\temp\unmanagedattached.csv -append -notypembuormation
+       }
+        }
+         }
+          }
